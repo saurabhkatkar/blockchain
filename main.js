18d9472ba57f69b9174a83e44d3a18437717c12c
Saurabh Katkar <saurabh.katkar102@gmail.com>
2020-03-29 00:57:06 +0530

    New changes

--- main.js
+++ main.js
@@ -9,18 +9,27 @@ class Block{
         _(this).data = data;                //Private
         this.previousHash = previousHash;   //Public
         this.hash = this.calculateHash();   //Public
+        _(this).nounce = 0;                 //Private value mining
     }
     calculateHash(){
-        return SHA256(_(this).index + this.previousHash + _(this).timestamp + JSON.stringify(_(this).data)).toString();
+        return SHA256(_(this).index + this.previousHash + _(this).timestamp + JSON.stringify(_(this).data) + _(this).nounce).toString();
     }
     showBlock(){
         console.log(_(this));
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty + 1).join('0')){
+            _(this).nounce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block Minied : " + this.hash);
     }
 }
 
 class BlockChain{
     constructor(){
         _(this).chain = [this.createGenesisBlock()];    //PRIVATE
+        _(this).difficulty = 3;                        //PRIVATE Adjust Dificulty
     }
     createGenesisBlock(){
         return new Block(0,"01/03/2020","Genesis Block","0");
@@ -30,12 +39,12 @@ class BlockChain{
     }
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(_(this).difficulty);
+        // newBlock.hash = newBlock.calculateHash();
         _(this).chain.push(newBlock);
-
     }
     showBlockChain(){
-        console.log(JSON.stringify(_(this),null,4));
+        console.log(JSON.stringify(_(this).chain,null,4));
     }
     checkHashes(){
         for (let i = 1; i< _(this).chain.length; i++){
@@ -55,7 +64,13 @@ class BlockChain{
 }
 
 let skCoin = new BlockChain();
+
+console.log("Block 1 added .......");
+
 skCoin.addBlock(new Block('1','10/03/2020',{amount : 3}));
+
+console.log("Block 2 added .......");
+
 skCoin.addBlock(new Block('2','20/03/2020',{amount : 10}));
 if( skCoin.checkHashes){
     console.log("Data is not Tampered");
